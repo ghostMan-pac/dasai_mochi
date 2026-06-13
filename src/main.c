99a99403fbaea05ed173e3da410e2ffbeab9c517
Fabin V Martin <Fabinv.Martin@microchip.com>
2026-06-13 17:13:38 +0530

    frame files

--- src/main.c
+++ src/main.c
@@ -9,13 +9,79 @@
 #include <zephyr/drivers/display.h>
 #include <zephyr/kernel.h>
 #include <lvgl.h>
-#include "widgets/gif/lv_gif.h"
 
 #define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(app);
 
-LV_IMG_DECLARE(squint_gif);
+#define DECLARE_FRAME(n) LV_IMG_DECLARE(squint_frame_##n)
+
+DECLARE_FRAME(000);
+DECLARE_FRAME(001);
+DECLARE_FRAME(002);
+DECLARE_FRAME(003);
+DECLARE_FRAME(004);
+DECLARE_FRAME(005);
+DECLARE_FRAME(006);
+DECLARE_FRAME(007);
+DECLARE_FRAME(008);
+DECLARE_FRAME(009);
+DECLARE_FRAME(010);
+DECLARE_FRAME(011);
+DECLARE_FRAME(012);
+DECLARE_FRAME(013);
+DECLARE_FRAME(014);
+DECLARE_FRAME(015);
+DECLARE_FRAME(016);
+DECLARE_FRAME(017);
+DECLARE_FRAME(018);
+DECLARE_FRAME(019);
+DECLARE_FRAME(020);
+DECLARE_FRAME(021);
+DECLARE_FRAME(022);
+DECLARE_FRAME(023);
+DECLARE_FRAME(024);
+DECLARE_FRAME(025);
+DECLARE_FRAME(026);
+DECLARE_FRAME(027);
+DECLARE_FRAME(028);
+DECLARE_FRAME(029);
+DECLARE_FRAME(030);
+DECLARE_FRAME(031);
+DECLARE_FRAME(032);
+DECLARE_FRAME(033);
+DECLARE_FRAME(034);
+DECLARE_FRAME(035);
+DECLARE_FRAME(036);
+DECLARE_FRAME(037);
+DECLARE_FRAME(038);
+DECLARE_FRAME(039);
+
+static const lv_image_dsc_t *frames[] = {
+	&squint_frame_000, &squint_frame_001, &squint_frame_002, &squint_frame_003,
+	&squint_frame_004, &squint_frame_005, &squint_frame_006, &squint_frame_007,
+	&squint_frame_008, &squint_frame_009, &squint_frame_010, &squint_frame_011,
+	&squint_frame_012, &squint_frame_013, &squint_frame_014, &squint_frame_015,
+	&squint_frame_016, &squint_frame_017, &squint_frame_018, &squint_frame_019,
+	&squint_frame_020, &squint_frame_021, &squint_frame_022, &squint_frame_023,
+	&squint_frame_024, &squint_frame_025, &squint_frame_026, &squint_frame_027,
+	&squint_frame_028, &squint_frame_029, &squint_frame_030, &squint_frame_031,
+	&squint_frame_032, &squint_frame_033, &squint_frame_034, &squint_frame_035,
+	&squint_frame_036, &squint_frame_037, &squint_frame_038, &squint_frame_039,
+};
+#define FRAME_COUNT (sizeof(frames) / sizeof(frames[0]))
+
+static lv_obj_t *img;
+static uint8_t current_frame = 0;
+
+static void anim_cb(lv_timer_t *t)
+{
+	current_frame++;
+	if (current_frame >= FRAME_COUNT) {
+		current_frame = 0;
+	}
+	lv_image_set_src(img, frames[current_frame]);
+}
 
 int main(void)
 {
@@ -34,10 +100,13 @@ int main(void)
 		return 0;
 	}
 
-	/* Create GIF object */
-	lv_obj_t *gif = lv_gif_create(lv_screen_active());
-	lv_gif_set_src(gif, &squint_gif);
-	lv_obj_center(gif);
+	/* Create image object */
+	img = lv_image_create(lv_screen_active());
+	lv_image_set_src(img, frames[0]);
+	lv_obj_center(img);
+
+	/* Create animation timer - 100ms per frame = 10 FPS */
+	lv_timer_create(anim_cb, 100, NULL);
 
 	while (1) {
 		lv_timer_handler();
